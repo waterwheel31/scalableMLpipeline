01dc507ae8e00023b0d579599ed32581417c1599
watherwheel <waterwheel31@gmail.com>
2021-08-19 18:42:07 +0900

    test added

---
+++ test/test_api.py
@@ -0,0 +1,14 @@
+
+
+
+def test_api1():
+
+    assert True 
+
+def test_api2():
+
+    assert True 
+
+def test_api3():
+
+    assert True 
---
+++ test/test_model.py
@@ -0,0 +1,14 @@
+
+
+
+def test_test1():
+
+    assert True 
+
+def test_test2():
+
+    assert True 
+
+def test_test3():
+
+    assert True 
